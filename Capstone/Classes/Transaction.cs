1319e2b361e326f51f8bcf947449f45c6306c29c
salva-dali2997 <salvatore1004@gmail.com>
2021-06-03 16:37:44 -0400

    Added Constructor to Transaction

--- Capstone/Classes/Transaction.cs
+++ Capstone/Classes/Transaction.cs
@@ -7,7 +7,11 @@ namespace Capstone.Classes
     public class Transaction
     {
         public decimal Balance { get; private set; } = 0.00M;
+        
+        public Transaction()
+        {
 
+        }
         /// <summary>
         /// Enters the amount into the balance of
         /// the current transaction
