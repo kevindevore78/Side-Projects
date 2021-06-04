210bc1d2f3eb4bc9752762da60da2ef146e8ea86
Kevin Devore <shootdogg1@gmail.com>
2021-06-04 10:01:27 -0400

    Chip changes

--- Capstone/Program.cs
+++ Capstone/Program.cs
@@ -10,19 +10,20 @@ namespace Capstone
         static void Main(string[] args)
         {
             VendingMachine myVendingMachine = new VendingMachine();
-            Chips one = new Chips("Cheetos", 1.00M);
+            Chip one = new Chip("Cheetos", 1.00M);
             Inventory inventory1 = new Inventory(one, 5);
             myVendingMachine.Stock.Add("a1", inventory1);
-            Chips two = new Chips("Lays", 1.50M);
+            Chip two = new Chip("Lays", 1.50M);
             Inventory inventory2 = new Inventory(two, 5);
             myVendingMachine.Stock.Add("a2", inventory2);
-            Chips three = new Chips("Pringles", 2.00M);
+            Chip three = new Chip("Pringles", 2.00M);
             Inventory inventory3 = new Inventory(three, 5);
             myVendingMachine.Stock.Add("a3", inventory3);   // Need to display slot number
 
 
             while (true)
             {
+                // Generates header
                 string vendHeader = FiggleFonts.Standard.Render("                    Vendo-Matic 800");
                 // Creation of main menu
                 Console.WriteLine(vendHeader);
@@ -33,22 +34,20 @@ namespace Capstone
                 Console.CursorLeft = 45;
                 Console.WriteLine("(3) Exit");
                 Console.CursorLeft = 50;
-                string userSelection = Console.ReadLine();
+                string userSelection = Console.ReadLine(); // Main menu selection 
                 Console.Clear();
 
                 while (true)
 
                     if (userSelection == "1")
                     {
-                        // Print all items with their quantity remaining
-                        Console.WriteLine(vendHeader);
-                        foreach (KeyValuePair<string, Inventory> saleItems in myVendingMachine.Stock)
+                        Console.WriteLine(vendHeader); // Generates header
+                        foreach (KeyValuePair<string, Inventory> saleItems in myVendingMachine.Stock) // Print all items with their quantity remaining
                         {
                             Console.CursorLeft = 45;
-                            Console.WriteLine(saleItems.Value.Product.Name + " | " + "$" + (saleItems.Value.Product.Price) + " | quantity = " + saleItems.Value.Stock);
-
+                            Console.WriteLine(saleItems.Key.Substring(0) + " | " + saleItems.Value.Product.Name + " | " + "$" + (saleItems.Value.Product.Price) + " | quantity = " + saleItems.Value.Stock);
+                            
                         }
-
                         Console.WriteLine();
                         Console.WriteLine();
                         Console.CursorLeft = 45;
@@ -62,7 +61,7 @@ namespace Capstone
                     {
 
                         // Guiding through the purchasing process menu
-                        Console.WriteLine(vendHeader);
+                        Console.WriteLine(vendHeader);// Generates header
                         Console.CursorLeft = 45;
                         Console.WriteLine("(1) Add Money");
                         Console.CursorLeft = 45;
@@ -75,21 +74,25 @@ namespace Capstone
                         Console.WriteLine($"Current Balance: {myVendingMachine.Transaction.Balance}"); //Populate current balance = $0 to start -- transaction class tie
                         Console.WriteLine();
                         Console.CursorLeft = 55;
-                        string purchaseSelection = Console.ReadLine();
+                        string purchaseSelection = Console.ReadLine(); // Where menu selection is entered
                         Console.Clear();
 
                         if (purchaseSelection == "1")
                         {
                             // Add Money to transaction class -- transaction class tie
-                            Console.WriteLine(vendHeader);
+                            Console.WriteLine(vendHeader); // Generates header
                             Console.CursorLeft = 35;
                             Console.WriteLine("Please add $1, $2, $5, or $10 bills and press 'Enter'");
                             Console.WriteLine();
                             Console.CursorLeft = 50;
-                            Console.WriteLine($"Current Balance: {myVendingMachine.Transaction.Balance}");
+                            Console.WriteLine($"Current Balance: {myVendingMachine.Transaction.Balance}"); // gets customer balance and displays
                             Console.WriteLine();
                             Console.CursorLeft = 60;
-                            string moneyAdded = Console.ReadLine();
+                            string moneyAdded = Console.ReadLine(); // Where money is entered
+                            //if ((moneyAdded == )
+                            //{
+                            //    myVendingMachine.Transaction.FeedMoney(decimal.Parse(moneyAdded)); // Error if 'Enter' is pressed and no number is entered
+                            //}
                             myVendingMachine.Transaction.FeedMoney(decimal.Parse(moneyAdded)); // Error if 'Enter' is pressed and no number is entered
                             Console.Clear();
 
@@ -98,7 +101,7 @@ namespace Capstone
                         {
                             // Select Product list populates
                             // User can select an option. The users balance follows through out
-                            Console.WriteLine(vendHeader);
+                            Console.WriteLine(vendHeader); // Generates header
                             Console.CursorLeft = 45;
                             Console.WriteLine("(1) Chips");
                             Console.CursorLeft = 45;
@@ -111,42 +114,44 @@ namespace Capstone
                             Console.WriteLine("(5) Return to previous menu");
                             Console.WriteLine();
                             Console.CursorLeft = 45;
-                            Console.WriteLine($"Current Balance: {myVendingMachine.Transaction.Balance}");
+                            Console.WriteLine($"Current Balance: {myVendingMachine.Transaction.Balance}"); // gets customer balance and displays
                             Console.WriteLine();
                             Console.CursorLeft = 60;
-                            string productSelection = Console.ReadLine();
+                            string productSelection = Console.ReadLine(); // Where product is selected 
                             Console.Clear();
 
                             if (productSelection == "1")
                             {
                                 // populate the list of chips available
-                                Console.WriteLine(vendHeader);
+                                Console.WriteLine(vendHeader); // Generates header
                                 Console.CursorLeft = 45;
-                                Console.WriteLine($"Current Balance: {myVendingMachine.Transaction.Balance}");
+                                Console.WriteLine($"Current Balance: {myVendingMachine.Transaction.Balance}"); // gets customer balance and displays
                                 Console.WriteLine();
                                 Console.CursorLeft = 45;
                                 Console.WriteLine("Place holder for chip menu");
+
                                 Console.ReadLine();
                                 Console.Clear();
                             }
                             else if (productSelection == "2")
                             {
                                 // populate the Candy available
-                                Console.WriteLine(vendHeader);
+                                Console.WriteLine(vendHeader); // Generates header
                                 Console.CursorLeft = 45;
-                                Console.WriteLine($"Current Balance: {myVendingMachine.Transaction.Balance}");
+                                Console.WriteLine($"Current Balance: {myVendingMachine.Transaction.Balance}"); // gets customer balance and displays
                                 Console.WriteLine();
                                 Console.CursorLeft = 45;
                                 Console.WriteLine("Place holder for candy menu");
+                                Console.CursorLeft = 45;
                                 Console.ReadLine();
                                 Console.Clear();
                             }
                             else if (productSelection == "3")
                             {
                                 //populate the Drinks available 
-                                Console.WriteLine(vendHeader);
+                                Console.WriteLine(vendHeader); // Generates header
                                 Console.CursorLeft = 45;
-                                Console.WriteLine($"Current Balance: {myVendingMachine.Transaction.Balance}");
+                                Console.WriteLine($"Current Balance: {myVendingMachine.Transaction.Balance}"); // gets customer balance and displays
                                 Console.WriteLine();
                                 Console.CursorLeft = 45;
                                 Console.WriteLine("Place holder for drinks available");
@@ -156,9 +161,9 @@ namespace Capstone
                             else if (productSelection == "4")
                             {
                                 // populate gum available 
-                                Console.WriteLine(vendHeader);
+                                Console.WriteLine(vendHeader); // Generates header
                                 Console.CursorLeft = 45;
-                                Console.WriteLine($"Current Balance: {myVendingMachine.Transaction.Balance}");
+                                Console.WriteLine($"Current Balance: {myVendingMachine.Transaction.Balance}"); // gets customer balance and displays
                                 Console.WriteLine();
                                 Console.CursorLeft = 45;
                                 Console.WriteLine("Place holder for gum");
@@ -175,7 +180,7 @@ namespace Capstone
                         {
                             //retrun to main menu
                             //return change due 
-                            Console.WriteLine(vendHeader);
+                            Console.WriteLine(vendHeader); // Generates header
                             Console.CursorLeft = 40;
                             Console.WriteLine("Here is your change due in the appropiate coins!");
                             Console.WriteLine();
@@ -192,14 +197,14 @@ namespace Capstone
                     else if (userSelection == "3")
                     {
                         // Exit program
-                        string ending = FiggleFonts.Tombstone.Render("Thanks for giving us your money!");
+                        string ending = FiggleFonts.Tombstone.Render("Thanks for giving us your money!"); //Creates ending text
                         Console.WriteLine(ending);
                         return;
                     }
                     else if (userSelection == "4")
                     {
                         // Hidden menu that displays all transactions 
-                        Console.WriteLine(vendHeader);
+                        Console.WriteLine(vendHeader); // Generates header
                         Console.CursorLeft = 45;
                         Console.WriteLine("Hidden Menu to Display Transaction Log");
                         Console.WriteLine();
@@ -210,7 +215,7 @@ namespace Capstone
                         break;
 
                     }
-                    else if ((userSelection!= "1")||(userSelection!= "2")|| (userSelection != "3")||(userSelection!="4")) //Logic to avoid entering invalid choice
+                    else if ((userSelection != "1") || (userSelection != "2") || (userSelection != "3") || (userSelection != "4")) //Logic to avoid entering invalid choice
                     {
                         break;
                     }
