16a89240455e07ac862cbd0aee2376111ac85803
salva-dali2997 <salvatore1004@gmail.com>
2021-06-04 16:27:06 -0400

    Again

--- Capstone/Classes/Logger.cs
+++ Capstone/Classes/Logger.cs
@@ -15,7 +15,7 @@ namespace Capstone.Classes
             {
                 using (StreamWriter write = new StreamWriter(logFile, true))
                 {
-                    write.WriteLine($"{DateTime.Today.Date.Date} {DateTime.Now} {transactionType}: " +
+                    write.WriteLine($"{DateTime.Now.Date.DayOfWeek} {DateTime.Now} {transactionType}: " +
                         $"${value1} ${value2}");
                 }
             }
