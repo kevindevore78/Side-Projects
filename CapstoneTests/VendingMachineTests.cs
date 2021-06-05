7c80c94d9732853c5e56fac4c47052f69da9085b
Kevin Devore <shootdogg1@gmail.com>
2021-06-05 13:54:49 -0400

    Test update 2:00pm

--- CapstoneTests/VendingMachineTests.cs
+++ CapstoneTests/VendingMachineTests.cs
@@ -1,11 +1,32 @@
-﻿using System;
+﻿using Capstone.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CapstoneTests
 {
-    class VendingMachineTests
+    [TestClass]
+    public class VendingMachineTests
     {
+        [TestMethod]
+        [DataRow(5,4, DisplayName = "Test that stock will lower one")]
+        [DataRow(1,0, DisplayName = "Test that stock will lower one")]
+        [DataRow(0,0, DisplayName = "Test that stock will not go lower than zero")]
+        public void VendTestSenarios(int stockQuantity, int expected)
+        {
+            VendingMachine vendingMachine = new VendingMachine();
+            Chip chip = new Chip("pringles", 1.50M, "chip");
+            Inventory inventory = new Inventory(chip, stockQuantity);
+
+            vendingMachine.Stock.Add("A1", inventory);
+
+            vendingMachine.Vend("A1");
+
+            Assert.AreEqual(expected, vendingMachine.Stock["A1"].Stock);
+            
+        }
+    
 
         
     }
